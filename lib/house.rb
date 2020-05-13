9f04a26b27ac24205ba78bb82696e6da6f9bcd7c
Judith Pillado <judith.pillado@gmail.com>
2020-05-13 11:28:24 -0500

    Add iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -5,10 +5,40 @@ class House
     @price = price.tr('$', '').to_i
     @address = address
     @rooms = []
+    @above_market_average = false
   end
 
   def add_room(room)
     @rooms << room
-  end 
+  end
+
+  def above_market_average?
+    if price > 500000
+      true
+    else
+      @above_market_average
+    end
+  end
+
+  def rooms_from_category(category)
+    rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    sum = 0
+    @rooms.each do |room|
+      sum += room.area
+    end
+    sum
+  end
+
+  def house_details
+    details = Hash.new
+    details["price"] = @price
+    details["address"] = @address
+    details
+  end
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -7,8 +7,11 @@ class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @house2 = House.new("$600000", "321 sugar lane")
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
   end
 
   def test_it_exists
@@ -28,6 +31,33 @@ class HouseTest < Minitest::Test
     @house.add_room(@room_1)
     @house.add_room(@room_2)
     assert_equal [@room_1, @room_2], @house.rooms
+  end
+
+  def test_if_it_is_above_market_average
+    assert_equal false, @house.above_market_average?
+    assert_equal true, @house2.above_market_average?
+  end
+
+  def test_it_can_get_rooms_from_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [@room_3], @house.rooms_from_category(:living_room)
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+  end
+
+  def test_house_area
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    assert_equal 1900, @house.area
+  end
+
+  def test_house_details
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), @house.house_details
   end
 
 end
